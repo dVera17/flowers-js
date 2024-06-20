9bc3bc7b786eff3d3b6c08166f9b8f76f50a90bb
dVera17 <dsvera174@gmail.com>
2024-06-19 21:35:42 -0500

    feat: :sparkles: sdsd
    sdsd

--- anim.js
+++ anim.js
@@ -1,39 +1,30 @@
-// Sincronizar las letras con la canción
+
 var audio = document.querySelector("audio");
 var lyrics = document.querySelector("#lyrics");
 
-// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+
 var lyricsData = [
-  { text: "At the time", time: 15 },
-  { text: "The whisper of birds", time: 18 },
-  { text: "Lonely before the sun cried", time: 27 },
-  { text: "Fell from the sky", time: 32 },
-  { text: "Like water drops", time: 33 },
-  { text: "Where I'm now? I don't know why", time: 41 },
-  { text: "Nice butterflies in my hands", time: 47 },
-  { text: "Too much light for twilight", time: 54 },
-  { text: "In the mood for the flowers love", time: 59 },
-  { text: "That vision", time: 67 },
-  { text: "Really strong, blew my mind", time: 72 },
-  { text: "Silence Let me see what it was", time: 78 },
-  { text: "I only want to live in clouds", time: 83 },
-  { text: "Where I'm now? I don't know why", time: 91 },
-  { text: "Nice butterflies in my hands", time: 97 },
-  { text: "Too much light for twilight", time: 104 },
-  { text: "In the mood for the flowers love", time: 108 },
-  { text: "At the time", time: 144 },
-  { text: "The whisper of birds", time: 148 },
-  { text: "Lonely before the sun cried", time: 153 },
-  { text: "Fell from the sky", time: 158 },
-  { text: "Like water drops", time: 164 },
-  { text: "Where I'm now? I don't know why", time: 169 },
-  { text: "Nice butterflies in my hands", time: 176 },
-  { text: "Too much light for twilight", time: 183 },
-  { text: "In the mood for the flowers", time: 188 },
-  { text: "Love.", time: 140 },
+  { text: "Valentina", time: 5 },
+  { text: "O mas bien cachetitos", time: 10 },
+  { text: "Se que hemos pasado por muchas etapas", time: 15 },
+  { text: "Hemos vivido momentos hermosos", time: 20 },
+  { text: "y otros no tanto", time: 25 },
+  { text: "Pero quiero que sepas", time: 30 },
+  { text: "que nunca, lee bien, NUNCA", time: 35 },
+  { text: "voy a dejar de luchar por los dos", time: 40 },
+  { text: "por ti", time: 45 },
+  { text: "Te amo", time: 50 },
+  { text: "Te amo de todas las formas posibles", time: 55 },
+  { text: "Y siempre te amare", time: 60 },
+  { text: "recuerda siempre eso", time: 65 },
+  { text: "y si esto es de parte de Dios", time: 70 },
+  { text: "Se nos va a dar", time: 75 },
+  { text: "Te amo infinitamente", time: 80 },
+  { text: "Y eres lo mejor que me ha pasado", time: 85 },
+  { text: "Te amo", time: 90 }
 ];
 
-// Animar las letras
+
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
   var currentLine = lyricsData.find(
@@ -41,32 +32,26 @@ function updateLyrics() {
   );
 
   if (currentLine) {
-    // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var fadeInDuration = 0.1; 
     var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
 
-    // Aplica el efecto de aparición
+    
     lyrics.style.opacity = opacity;
     lyrics.innerHTML = currentLine.text;
   } else {
-    // Restablece la opacidad y el contenido si no hay una línea actual
     lyrics.style.opacity = 0;
     lyrics.innerHTML = "";
   }
 }
 
 setInterval(updateLyrics, 1000);
-
-//funcion titulo
-// Función para ocultar el título después de 216 segundos
 function ocultarTitulo() {
   var titulo = document.querySelector(".titulo");
   titulo.style.animation =
-    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+    "fadeOut 3s ease-in-out forwards";
   setTimeout(function () {
     titulo.style.display = "none";
-  }, 3000); // Espera 3 segundos antes de ocultar completamente
+  }, 3000);
 }
 
-// Llama a la función después de 216 segundos (216,000 milisegundos)
 setTimeout(ocultarTitulo, 216000);
